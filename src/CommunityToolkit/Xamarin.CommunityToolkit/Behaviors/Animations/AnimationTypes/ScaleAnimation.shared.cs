ae35e514e5337de3f4c9042e0d4d1e9a36202a58
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-13 19:49:53 +0100

    Add more animation behaviors

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Animations/AnimationTypes/ScaleAnimation.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Animations/AnimationTypes/ScaleAnimation.shared.cs
@@ -22,4 +22,22 @@ namespace Xamarin.CommunityToolkit.Behaviors
 			await view.ScaleTo(1, Duration, Easing);
 		}
 	}
+
+	public class RelScaleAnimation : AnimationBase
+	{
+		public static readonly BindableProperty ScaleProperty =
+			BindableProperty.Create(nameof(Scale), typeof(double), typeof(RelScaleAnimation), default(double),
+				BindingMode.TwoWay, null);
+
+		public double Scale
+		{
+			get => (double)GetValue(ScaleProperty);
+			set => SetValue(ScaleProperty, value);
+		}
+
+		protected override uint DefaultDuration { get; set; } = 170;
+
+		public override async Task Animate(View view) =>
+			await view.RelScaleTo(Scale, Duration, Easing);
+	}
 }
