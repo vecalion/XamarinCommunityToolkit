829479e01a2a568b8f81488976f824b7fad88623
Valeriy Kovalenko <valeriy.kovalenko@squarecrowdapps.com>
2021-03-25 09:55:56 +0200

    repro for a tabview bug

--- samples/XCT.Sample/Pages/Views/TabView/GettingStartedPage.xaml.cs
+++ samples/XCT.Sample/Pages/Views/TabView/GettingStartedPage.xaml.cs
@@ -1,7 +1,34 @@
-﻿namespace Xamarin.CommunityToolkit.Sample.Pages.Views.TabView
+﻿using System;
+using System.Diagnostics;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.Views.TabView
 {
 	public partial class GettingStartedPage : BasePage
 	{
-		public GettingStartedPage() => InitializeComponent();
+		public GettingStartedPage()
+		{
+			BindingContext = new FirstViewModel();
+			InitializeComponent();
+		}
+	}
+
+	public class FirstViewModel : ObservableObject
+	{
+		int index;
+		public int Index
+		{
+			get => index;
+			set
+			{
+				SetProperty(ref index, value);
+				Debug.WriteLine($"{DateTime.Now} - New index: {value}");
+			}
+		}
+
+		public FirstViewModel()
+		{
+			Index = 1;
+		}
 	}
 }
